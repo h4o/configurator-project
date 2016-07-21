0cadfafb159295dd608a2f69c3093cd186db0e65
pol-vicent <fabien.vicente@etu.unice.fr>
2016-07-21 14:25:43 +0200

    updated postgres to add support for jira

--- assets/postgres/Generator.py
+++ assets/postgres/Generator.py
@@ -17,6 +17,9 @@ class Generator:
 		if('bitbucket' in  conf['linked-config']):
 			conf['config']['bitbucket'] = conf['linked-config']['bitbucket']
 			conf['config']['bitbucket']['license'] = conf['config']['bitbucket']['license'].replace('\n','\\')
+		if('jira' in  conf['linked-config']):
+			conf['config']['jira'] = conf['linked-config']['jira']
+			conf['config']['jira']['license'] = conf['config']['jira']['license'].replace('\n','\\')
 		if('crowd' in conf['linked-config']):
 			conf['config']['crowd'] = conf['linked-config']['crowd']
 		out = template.render(config['config'])
@@ -35,6 +38,11 @@ class Generator:
 			template = env.get_template('crowd.sql')
 			out = template.render(config['config'])
 			with open(dest+"crowd.sql", "w") as fh:
+				fh.write(out)
+		if('jira' in conf['linked-config']):
+			template = env.get_template('jira.sql')
+			out = template.render(config['config'])
+			with open(dest+"jira.sql", "w") as fh:
 				fh.write(out)
 
 
