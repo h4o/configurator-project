95d7b551196e10651d142a531744d9cb8c2cc4f5
pol-vicent <fabien.vicente@etu.unice.fr>
2016-07-25 14:25:38 +0200

    added back volumes to the system

--- template/up.sh
+++ template/up.sh
@@ -2,5 +2,11 @@
 
 {% for image in images %}
 docker-compose build --no-cache {{image}}
+{% if image.volumes %}
+  {% for volume in image.volumes %}
+  mkdir -R /etc/data/{{ image.name }}/{{ volume.name }}
+  chmod 777 /etc/data/{{ image.name }}/{{ volume.name }}
+  {% endfor %}
+{% endif %}
 {% endfor %}
 docker-compose up
