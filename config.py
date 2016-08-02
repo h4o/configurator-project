fde82aacd4d132f4dd4579086ba3b8e0ade6cf37
pol-vicent <fabien.vicente@etu.unice.fr>
2016-08-02 14:57:42 +0200

    Reformatting of the main script

--- config.py
+++ config.py
@@ -1,58 +1,56 @@
 #!/bin/python3
-import sys
-import os
 import argparse
 from MetaLoader import MetaLoader
 from pydoc import locate
-from jinja2 import Environment, FileSystemLoader,Template
+from jinja2 import Environment, FileSystemLoader, Template
 import yaml
 import copy
-import AbstractGenerator
 
 
 def iterateDependency(imgs):
 	imageList = {}
-	while imgs:#an empty list is falsey
+	while imgs:  # an empty list is falsey
 		imgSwp = []
 		for img in imgs:
-			
-			if(img != 'config.py'):
-				imageList[img] =  MetaLoader('assets/'+img+'/')
+
+			if img != 'config.py':
+				imageList[img] = MetaLoader('assets/' + img + '/')
 				if imageList[img].getRequiredImages():
 					for image in imageList[img].getRequiredImages():
 						if image not in imgs and image not in imageList and image not in imgSwp:
-							imgSwp.append(image);
+							imgSwp.append(image)
 		imgs = imgSwp
 
 	return imageList
 
-def askForConfig(imageList,configFile):
+
+def askForConfig(imageList, configFile):
 	base_ip = '173.17.0.'
 	ip = 2
-	configList = {'global':{}}
+	configList = {'global': {}}
 	configInfo = {}
 	config = {}
 	configData = {}
-	if(configFile):
-		with open(configFile,"r") as stream:
+	if (configFile):
+		with open(configFile, "r") as stream:
 			try:
 				configData = yaml.load(stream)
 			except yaml.YAMLError as exc:
 				print(exc)
 
 	for image in imageList:
-		configInfo[image] = imageList[image].getRequiredData()			
+		configInfo[image] = imageList[image].getRequiredData()
 	for el in configInfo:
-		print('asking config for image '+el)
+		print('asking config for image ' + el)
 		config[el] = {}
-		config[el]['ip'] = base_ip+str(ip)
+		config[el]['ip'] = base_ip + str(ip)
 		ip += 1
 		for quest in configInfo[el]:
-			if(configFile and el in configData and quest in configData[el]):
+			if configFile and el in configData and quest in configData[el]:
 				config[el][quest] = configData[el][quest]
 			else:
 				config[el][quest] = input(quest + "?")
-			
+
 			if 'from' in configInfo[el][quest] and configInfo[el][quest]['from'] == 'filesystem':
 				readType = 'r'
 				if configInfo[el][quest]['type'] == 'binary':
@@ -61,17 +59,15 @@ def askForConfig(imageList,configFile):
 					with open(config[el][quest], readType) as content_file:
 						config[el][quest] = content_file.read()
 				except Exception as E:
-					if(configInfo[el][quest]['necessary']):
+					if configInfo[el][quest]['necessary']:
 						raise E
 					else:
 						pass
 
-
-
-
 	return config
 
-def callGenerators(imageList,config):
+
+def callGenerators(imageList, config):
 	###we save data in a dict
 	###there is 3 parts:
 	### imageList: the list of image that are to be deployed
@@ -87,18 +83,19 @@ def callGenerators(imageList,config):
 		data['imageList'] = imageMeta
 		data['config'] = config[image]
 		data['linked-config'] = {}
-		print("connectors: "+str(imageList[image].getConnectors()))
+		print("connectors: " + str(imageList[image].getConnectors()))
 		for connector in imageList[image].getConnectors():
-			print('searching for connector '+connector)
+			print('searching for connector ' + connector)
 			if connector in imageList:
-				data['linked-config'][connector]= config[connector]
+				data['linked-config'][connector] = config[connector]
 		###we use reflexivity to load the generator pertaining to the image
 		print('CALLING GENERATOR FOR ' + image)
-		g = locate('assets.'+image+'.Generator')
-		gen = g.Generator('assets/'+image+'/', 'output/'+image+'/')
+		g = locate('assets.' + image + '.Generator')
+		gen = g.Generator('assets/' + image + '/', 'output/' + image + '/')
 		gen.generate(copy.deepcopy(data))
 
-def generateComposition(imageList,config):
+
+def generateComposition(imageList, config):
 	imageNameList = list(imageList.keys())
 	imageMeta = []
 	for name in imageNameList:
@@ -106,36 +103,32 @@ def generateComposition(imageList,config):
 		meta['name'] = name
 		meta['ip'] = config[name]['ip']
 		imageMeta.append(meta)
-	print('====================META==============')
-	print(imageMeta)
 	env = Environment(loader=FileSystemLoader('template'))
 	template = env.get_template('docker-compose.yml')
-	out = template.render({'images':imageMeta})
+	out = template.render({'images': imageMeta})
 	with open("output/docker-compose.yml", "w") as fh:
 		fh.write(out)
 	template = env.get_template('up.sh')
-	out = template.render({'images':imageMeta})
+	out = template.render({'images': imageMeta})
 	with open("output/up.sh", "w") as fh:
 		fh.write(out)
+
+if __name__ == '__main__':
 	
-
-parser = argparse.ArgumentParser(description='Generates image and docker composition for you using its assets')
-parser.add_argument('-c','--config',help='Configuration file for options')
-parser.add_argument('-i','--images',help='Images to use',action='append',nargs='*',required=True)
-args = parser.parse_args()
-print(args)
-print("################################")
-print("Scanning the images...")
-imageList = iterateDependency(args.images[0])
-print("################################")
-print("Configuration Step")
-config = askForConfig(imageList,args.config)
-print("################################")
-print("Generating the image's files")
-callGenerators(imageList,config)
-print("################################")
-print("Generating the docker-compose file")
-generateComposition(imageList,config)
-
-
-
+	parser = argparse.ArgumentParser(description='Generates image and docker composition for you using its assets')
+	parser.add_argument('-c', '--config', help='Configuration file for options')
+	parser.add_argument('-i', '--images', help='Images to use', action='append', nargs='*', required=True)
+	args = parser.parse_args()
+	print(args)
+	print("################################")
+	print("Scanning the images...")
+	imageList = iterateDependency(args.images[0])
+	print("################################")
+	print("Configuration Step")
+	config = askForConfig(imageList, args.config)
+	print("################################")
+	print("Generating the image's files")
+	callGenerators(imageList, config)
+	print("################################")
+	print("Generating the docker-compose file")
+	generateComposition(imageList, config)
