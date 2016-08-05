63847c33ad31581c0b7b2a0f5836945979fe623e
root <root@sparks-vm5.i3s.unice.fr>
2016-08-05 10:56:29 +0200

    added nginx information for crowd's template generation

--- assets/crowd/Generator.py
+++ assets/crowd/Generator.py
@@ -10,6 +10,7 @@ class Generator(AbstractGenerator):
 		print("we generate our files !")
 		super().copy()
 		config['config']['postgres'] = config['linked-config']['postgres']
+		config['config']['nginx'] = config['linked-config']['nginx']
 		super().generate_template('crowd.properties', 'w', config['config'])
 		super().generate_template('crowd.cfg.xml', 'w', config['config'])
 		super().generate_template('server.xml','w',config['config'])
