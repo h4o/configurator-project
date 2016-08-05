77267e58fc469f29173c91f74a60a51aa6c04833
root <root@sparks-vm5.i3s.unice.fr>
2016-08-05 10:16:30 +0200

    added server.xml to the files of the generated image

--- assets/crowd/Generator.py
+++ assets/crowd/Generator.py
@@ -12,3 +12,4 @@ class Generator(AbstractGenerator):
 		config['config']['postgres'] = config['linked-config']['postgres']
 		super().generate_template('crowd.properties', 'w', config['config'])
 		super().generate_template('crowd.cfg.xml', 'w', config['config'])
+		super().generate_template('server.xml','w',config['config'])
