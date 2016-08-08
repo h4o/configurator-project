12e6a66291a256e20831cb4bb7dd4ba1bcca0daa
pol-vicent <fabien.vicente@etu.unice.fr>
2016-08-08 09:45:31 +0200

    minor source style modification

--- AbstractGenerator.py
+++ AbstractGenerator.py
@@ -1,13 +1,14 @@
-from distutils.dir_util import copy_tree
-from jinja2 import Environment, FileSystemLoader,Template
+from jinja2 import Environment, FileSystemLoader, Template
 from abc import ABC, abstractmethod
 import shutil
 import os
+
 
 class AbstractGenerator(ABC):
     def __init__(self, base, dest):
         self.base = base
         self.dest = dest
+
     @abstractmethod
     def generate(self, config, base, dest):
         pass
@@ -16,7 +17,7 @@ class AbstractGenerator(ABC):
         if os.path.exists(self.dest):
             shutil.rmtree(self.dest)
         pattern = shutil.ignore_patterns('template', 'template/*', 'Generator.py', '__pycache__', 'meta.yml')
-        shutil.copytree(self.base, self.dest,ignore=pattern)
+        shutil.copytree(self.base, self.dest, ignore=pattern)
 
     def generate_template(self,filename, writeType, data):
         env = Environment(loader=FileSystemLoader(self.base + 'template'))
